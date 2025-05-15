9b5c5adca2c76559232d9aa399f9ec5ea4d9790b
Hélène Rizzon <helenerizzon3@gmail.com>
2025-05-15 12:05:19 +0200

    Install compodoc + commentaires

--- src/app/pages/edit-product/edit-product.component.ts
+++ src/app/pages/edit-product/edit-product.component.ts
@@ -6,26 +6,50 @@ import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from '@angula
 import {HttpClient} from '@angular/common/http';
 import {MatOption, MatSelect, MatSelectModule} from '@angular/material/select';
 import {ActivatedRoute} from '@angular/router';
-import {MatSnackBar} from '@angular/material/snack-bar';
 import {NotificationService} from '../../services/notification.service';
 import {Etat} from '../../models/etat';
 
 // ReactiveFormsModule utilisé pour valider le formulaire
-// FormsModule bloque le formulaire pour ne pas recharger la page
+// FormsModule bloque le formulaire pour ne pas recharger la
+
+/**
+ * Composant responsable de la création et de l'édition d'un produit.
+ * Utilise Reactive Forms pour gérer la saisie utilisateur et Angular Material pour l'interface.
+ */
 @Component({
   selector: 'app-edit-product',
-  imports: [MatInputModule, MatButtonModule, MatIconModule,
-    MatSelectModule, ReactiveFormsModule, FormsModule, MatSelect, MatOption],
+  imports: [
+    MatInputModule, MatButtonModule, MatIconModule,
+    MatSelectModule, ReactiveFormsModule, FormsModule, MatSelect, MatOption
+  ],
   templateUrl: './edit-product.component.html',
   styleUrl: './edit-product.component.scss'
 })
 export class EditProductComponent implements OnInit {
 
+  /**
+   * Constructeur du formulaire réactif.
+   */
   formBuilder = inject(FormBuilder);
+
+  /**
+   * Service HTTP pour communiquer avec l'API.
+   */
   http = inject(HttpClient);
+
+  /**
+   * Service Angular permettant d'accéder aux paramètres de la route.
+   */
   activatedRoute = inject(ActivatedRoute);
+
+  /**
+   * Service personnalisé pour afficher des notifications.
+   */
   notification = inject(NotificationService);
 
+  /**
+   * Formulaire réactif pour la saisie ou modification d’un produit.
+   */
   formulaire = this.formBuilder.group({
     nom: ['Nouveau produit', [Validators.required, Validators.maxLength(20), Validators.minLength(3)]],
     codeArticle: ['777', [Validators.required]],
@@ -35,18 +59,33 @@ export class EditProductComponent implements OnInit {
     etiquettes: [[] as Etiquette[]],
   });
 
+  /**
+   * Liste des états possibles à sélectionner dans le formulaire.
+   */
   etats: Etat[] = []
+
+  /**
+   * Liste des étiquettes disponibles.
+   */
   etiquettes: Etiquette[] = [];
+
+  /**
+   * Produit actuellement en cours d'édition. Null en cas de création.
+   */
   productEdite: Product | null = null;
   // notification = inject(MatSnackBar);
 
-
+  /**
+   * Méthode appelée à l'initialisation du composant.
+   * - Charge les paramètres de route (édition ou création),
+   * - Récupère la liste des états et étiquettes disponibles.
+   */
   ngOnInit() {
 
     this.activatedRoute.params
       .subscribe(parametres => {
 
-        // Si c'est une édition
+        // Mode édition : chargement du produit existant
         if (parametres['id']) {
           this.http
             .get<Product>('http://localhost:8080/product/' + parametres['id'])
@@ -67,39 +106,45 @@ export class EditProductComponent implements OnInit {
 
   }
 
-
+  /**
+   * Soumet le formulaire pour ajouter ou modifier un produit.
+   * Affiche une notification selon le succès de l’opération.
+   */
   onAjoutProduct() {
 
     if (this.formulaire.valid) {
       // console.log(this.formulaire.value);
 
-      // Pour éditer produit
+      // Édition d’un produit existant
       if (this.productEdite) {
-
-        // const productArenvoyer = {...this.formulaire.value, createur : this.productEdite.createur} => MAUVAISE SOLUTION -> logique doit rester dans le back pour associer le créateur
-        // => Voir productController
-
-        // TODO notification 'produit modifié' ne s'affiche pas -> A corriger
         this.http
-          .put('http://localhost:8080/product' + this.productEdite.id, this.formulaire.value) // TODO chercher différence méthode post & put -> laquelle mieux ici ?
+          .put('http://localhost:8080/product' + this.productEdite.id, this.formulaire.value)
           .subscribe(resultat => {
             this.notification.show("Le produit a bien été modifié", "valid")
-          })
+          });
 
       } else {
 
         this.http
+          // Création d’un nouveau produit
           .post('http://localhost:8080/product', this.formulaire.value)
           .subscribe(resultat => {
             this.notification.show("Le produit a bien été ajouté", "valid")
-          })
+          });
       }
     }
 
   }
 
+  /**
+   * Compare deux objets par leur propriété `id`.
+   * Utilisé dans les listes déroulantes pour la sélection correcte.
+   *
+   * @param o1 Premier objet à comparer
+   * @param o2 Deuxième objet à comparer
+   * @returns true si les IDs sont identiques
+   */
   compareId(o1: {id: number}, o2: {id: number}) {
-
     return o1.id === o2.id;
   }
 
--- src/app/pages/liste-produits/liste-produits.component.ts
+++ src/app/pages/liste-produits/liste-produits.component.ts
@@ -1,7 +1,11 @@
-import {Component, inject} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {MatTable} from '@angular/material/table';
+import { Component, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MatTable } from '@angular/material/table';
 
+/**
+ * Composant responsable de l'affichage de la liste des produits.
+ * Utilise une table Angular Material pour présenter les données.
+ */
 @Component({
   selector: 'app-liste-produits',
   imports: [
@@ -12,15 +16,30 @@ import {MatTable} from '@angular/material/table';
 })
 export class ListeProduitsComponent {
 
+  /**
+   * Injection du service HttpClient pour effectuer des requêtes HTTP.
+   */
   http = inject(HttpClient);
 
+  /**
+   * Tableau contenant la liste des produits récupérés depuis l'API.
+   */
   products: Product[] = [];
-  displayedColumns= [
-    "nom", "codeArticle", "prix", "description", "createur"];
 
+  /**
+   * Colonnes affichées dans le tableau des produits.
+   */
+  displayedColumns = [
+    'nom', 'codeArticle', 'prix', 'description', 'createur'
+  ];
+
+  /**
+   * Méthode appelée à l'initialisation du composant.
+   * Récupère les produits depuis le backend et les assigne au tableau `products`.
+   */
   ngOnInit() {
     this.http
       .get<Product[]>('http://localhost:8080/products')
-      .subscribe(products => this.products = products)
+      .subscribe(products => this.products = products);
   }
 }
